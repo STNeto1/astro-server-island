6ce2abcb4f41ba7a0f90a62607db3942ff782d9e
STNeto1 <gfreitasneto18@gmail.com>
2024-07-19 13:50:04 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   experimental: {
-    serverIslands: true,
+    serverIslands: true
   },
+  adapter: netlify()
 });
